ab2fe9e85ec7c092d85dc607f039726d0163d245
Lagrang3 <eduardo.quintana@pm.me>
2021-08-09 14:22:09 +0200

    added the raw halfcomplex-to-real routine for powers of 2

--- include/boost/math/fft/real_algorithms.hpp
+++ include/boost/math/fft/real_algorithms.hpp
@@ -139,6 +139,74 @@
       }
     }
   }
+  template <class T>
+  void real_inverse_dft_power2(const T *in_first, const T *in_last, T* out, int sign)
+  {
+    /*
+      Cooley-Tukey mapping, in-place Decimation in Time 
+      Reverse flow graph of the real_dft_power2
+    */
+    const long ptrdiff = static_cast<long>(std::distance(in_first,in_last));
+    if(ptrdiff <=0 )
+      return;
+    const long n = lower_bound_power2(ptrdiff);
+    
+    if(in_first!=out)
+      std::copy(in_first,in_last,out);
+    
+    if (n == 1)
+        return;
+    
+    for (int len = n, prev_len = len/2; len >= 2; len >>= 1,prev_len>>=1)
+    {
+      for (int i = 0; i < n; i += len)
+      {
+        {
+          // j=0;
+          T* u = out + i, *v = out + i + prev_len;
+          T Bu = *u, Bv = *v;
+          *u = (Bu + Bv)*0.5;
+          *v = (Bu - Bv)*0.5;
+        }
+        for(int j=1;j < prev_len/2;++j)
+        {
+          T cos{ complex_root_of_unity_real<T>(len,j*sign) }, 
+            sin{ complex_root_of_unity_imag<T>(len,j*sign) };
+          
+          T *ux = out + i + j, 
+            *uy = out + i + len - j;
+            
+          T *vx = out + i + prev_len - j, 
+            *vy = out + i + prev_len + j;
+          
+          T prev_ux = *ux, 
+            prev_uy = *uy;
+            
+          T prev_vx = *vx, 
+            prev_vy = *vy;
+          
+          T sum_x = (prev_ux + prev_vx) * 0.5,
+            dif_x = (prev_ux - prev_vx) * 0.5,
+            sum_y = (prev_uy + prev_vy) * 0.5,
+            dif_y = (prev_uy - prev_vy) * 0.5;
+            
+          *ux = sum_x;
+          *vx = dif_y;
+          
+          *uy = cos * sum_y + sin*dif_x;
+          *vy = cos * dif_x - sin*sum_y;
+        }
+      }
+    }
+    
+    // Gold-Rader bit-reversal algorithm.
+    for(int i=0,j=0;i<n-1;++i)
+    { 
+      if(i<j)
+        std::swap(out[i],out[j]);
+      for(int k=n>>1;!( (j^=k)&k );k>>=1);
+    }
+  }
   
   } // namespace detail
 
