68d7cca16c9b9534b4c5da8652e80651d5444014
Lagrang3 <eduardo.quintana@pm.me>
2021-08-17 17:00:32 +0200

    testing fftw with un-aligned data in rfft

--- test/fft_alignment.cpp
+++ test/fft_alignment.cpp
@@ -37,15 +37,41 @@ void test()
   P.forward(B.data(),B.data()+N,B.data());
   P.backward(B.data(),B.data()+N,B.data());
 }
+template<class Backend, int N >
+void test_real()
+{
+  using T = typename Backend::value_type;
+  std::cout << "Testing: " << boost::core::demangle(typeid(T).name()) << "\n";
+  Backend P(N);    
+  alignas(1)  std::array<T,N> A;
+  alignas(16) std::array<T,N> B;
+  
+  P.real_to_halfcomplex(A.data(),A.data()+N,B.data());
+  P.halfcomplex_to_real(A.data(),A.data()+N,B.data());
+  
+  P.real_to_halfcomplex(A.data(),A.data()+N,A.data());
+  P.halfcomplex_to_real(A.data(),A.data()+N,A.data());
+  
+  P.real_to_halfcomplex(B.data(),B.data()+N,B.data());
+  P.halfcomplex_to_real(B.data(),B.data()+N,B.data());
+}
 
 int main()
 {
   using boost::math::fft::fftw_dft;
+  using boost::math::fft::fftw_rdft;
   test<fftw_dft<std::complex<float>>,      16 >();
   test<fftw_dft<std::complex<double>>,     16 >();
   test<fftw_dft<std::complex<long double>>,16 >();
 #ifdef BOOST_MATH_USE_FLOAT128
   test<fftw_dft<boost::multiprecision::complex128>,16 >();
+#endif
+  
+  test_real<fftw_rdft<float>,      16 >();
+  test_real<fftw_rdft<double>,     16 >();
+  test_real<fftw_rdft<long double>,16 >();
+#ifdef BOOST_MATH_USE_FLOAT128
+  test_real<fftw_rdft<boost::multiprecision::float128>,16 >();
 #endif
   return 0;
 }
