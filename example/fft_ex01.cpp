c1cee846c15ff0dcaa925ead5004d580648798f3
Lagrang3 <eduardo.quintana@pm.me>
2021-08-18 09:31:09 +0200

    simpler example 01 using aliases

--- example/fft_ex01.cpp
+++ example/fft_ex01.cpp
@@ -23,17 +23,18 @@ void print(const std::vector< std::complex<T> >& V)
 int main()
 {
     std::vector< std::complex<double> > A{1.0,2.0,3.0,4.0},B(A.size());
+    using fft_transform = ::boost::math::fft::bsl_transform;
     
     // default fft engine, forward transform, out-of-place
     // following is called, but with an alias bsl_transform
     // fft::transform<fft::bsl_dft<std::complex<double>>>::forward(A.cbegin(),A.cend(),B.begin());
-    fft::bsl_transform::forward(A.cbegin(),A.cend(),B.begin());
+    fft_transform::forward(A.cbegin(),A.cend(),B.begin());
     
     print(B);
     
     // default fft engine, backward transform, in-place
     // fft::transform<fft::bsl_dft<std::complex<double>>>::backward(B.cbegin(),B.cend(),B.begin());
-    fft::bsl_transform::backward(B.cbegin(),B.cend(),B.begin());
+    fft_transform::backward(B.cbegin(),B.cend(),B.begin());
     
     print(B);
     
